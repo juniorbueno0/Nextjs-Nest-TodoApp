ba9953b0661ff3939fddaf05995fd4ffb6d396a8
juniorbueno0 <jjunior.buen00@gmail.com>
2023-10-24 11:56:47 -0700

    f

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { useState, useEffect } from "react";
-import { createTodos, getTodos, Todos } from "@/lib/data";
+import { createTodos, deleteTodos, getTodos, Todos, Todo } from "@/lib/data";
+
 
 export default function Home() {
   // todo list
@@ -13,11 +14,13 @@ export default function Home() {
   const [status, setStatus] = useState('');
 
   useEffect(() => {
-    getTodos().then((todos: Todos[]) => setTodos(todos));
+    getTodos().then((todos: Todos[]) => {setTodos(todos); console.log(todos)}).catch((error) => {
+      console.log(error)
+    });
   }, []);
 
   function handleSubmit() {
-    const newPost: Todos = {
+    const newPost: Todo = {
       title,
       description,
       status
@@ -30,26 +33,78 @@ export default function Home() {
     });
   }
 
-  return (
-    <div>
-      <div className="">
-        <form onSubmit={handleSubmit}>
-        <div>
-          <input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
-          <input type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
-          <input type="text" placeholder="Status" onChange={(e) => setStatus(e.target.value)}/>
-        </div>
-          <button type="submit" className="bg-white text-green-300">Post</button>
-        </form>
+  function handleDelete(id: Todos){
+    deleteTodos(id)
+  }
 
+  return (
+    <div className="bg-white w-screen h-screen justify-center items-center flex flex-col gap-4">
+
+      <div className="flex gap-4 justify-between">
+        <div className="bg-indigo-300 w-[300px] h-[50px] flex flex-col items-center justify-center text-center rounded-lg">
+          <h2 className="text-center font-extrabold text-4xl">Create Todo</h2>
+        </div>
+        <div className="bg-indigo-300 h-[50px] w-[620px] rounded-xl flex items-center justify-center">
+        <h2 className="text-center font-extrabold text-4xl">Todo list</h2>
+        </div>
       </div>
 
-      <div>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.title}> {todo.title} - {todo.description} : {todo.status}</li>
-          ))}
-        </ul>
+      <div className="flex gap-4">
+        <div className="bg-black w-[300px] h-[250px] flex flex-col items-center text-center rounded-lg">
+          <form onSubmit={handleSubmit}>
+            <div className="text-white flex flex-col p-2 mt-4">
+              <input type="text" className="input input-bordered mt-1 h-[40px]" placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
+              <input type="text" className="input input-bordered mt-2 h-[40px]" placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
+              <input type="text" className="input input-bordered mt-2 h-[40px]" placeholder="Status" onChange={(e) => setStatus(e.target.value)}/>
+            </div>
+              <button type="submit" className="bg-white text-black mt-2 w-full rounded-2xl h-[50px]">Post</button>
+          </form>
+        </div>
+
+        <div className="bg-black h-[250px] w-[620px] rounded-xl flex">
+          <div className="overflow-x-auto">
+            <table className="table table-sm">
+              <thead>
+                <tr>
+                  <th>Task</th>
+                  <th></th>
+                  <th></th>
+                  <th>Description</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th>Status</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {todos.map((todo: Todos) => (
+                  <tr>
+                    <td>{todo.title}</td>
+                    <td></td>
+                    <td></td>
+                    <td>{todo.description}</td>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <td>{todo.status}</td>
+                    <td></td>
+                    <td></td>
+                    <td><button className="ml-4 bg-red-400 rounded-lg px-1 hover:bg-red-500" onClick={() => handleDelete(todo)}> Delete</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>    
   )
--- lib/data.ts
+++ lib/data.ts
@@ -6,7 +6,14 @@ type Post = {
     description:string
 }
 
+export type Todo = {
+    title: string
+    description:string
+    status:string
+}
+
 export type Todos = {
+    _id: string
     title: string
     description:string
     status:string
@@ -17,19 +24,41 @@ let posts: Post[] = [];
 
 // it works :)
 export async function getTodos(): Promise<Todos[]> {
-  const response = await axios.get('http://localhost:3001/todo');
-  return response.data;
+    const response = await axios.get('http://localhost:3002/todos');
+    console.log(response.data);
+//     const todosWithId: Todos[] = [];
+
+//   for (const todo of response.data) {
+//     todosWithId.push({
+//       ...todo,
+//       _id: todo._id
+//     });
+//   }
+//   console.log(todosWithId);
+  
+    return response.data;
+    // return todosWithId;
 }
 
-export async function createTodos(user: Todos){
-    // const response = await axios.get('http://localhost:3001/todo');
-    // return response.data;
+export async function createTodos(user: Todo){
+
     console.log(user)
-    axios.post('http://localhost:3001/todo', user)
+    axios.post('http://localhost:3002/todos', user)
     .then(() => {
         return 'OK';
     })
     .catch((error) => {
+        return error;
+    });
+}
+
+export function deleteTodos(id:Todos) {
+    const idd = id._id
+    console.log(idd);
+    
+    axios.delete('http://localhost:3002/todos/' + idd).then(() => {
+        return 'OK';
+    }).catch((error) => {
         return error;
     });
 }
